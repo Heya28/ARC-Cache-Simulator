0b67530490b8efdf4a3746735a680fe747737681
hiyat <hiyatrehan@gmail.com>
2026-08-11 20:01:02 +0000

    Fix: Hardcoded address decomposition to allowing different cache_sizes in AddressDecoder.cpp

--- include/AddressDecoder.hpp
+++ include/AddressDecoder.hpp
@@ -3,3 +3,4 @@
 
 uint32_t get_index(uint32_t address);
 uint32_t get_tag(uint32_t address);
+uint32_t get_offset(uint32_t address, uint32_t block_size);
--- src/AddressDecoder.cpp
+++ src/AddressDecoder.cpp
@@ -1,9 +1,31 @@
 #include "AddressDecoder.hpp"
 
-uint32_t get_index(uint32_t address) {
-    return (address >> 6) & 0x3F; // shift 6 bits and bitmask
+uint32_t log2_exact(uint32_t n) {
+    // for floating - point precision errors in log2 (cmath function)
+    uint32_t bits = 0;
+    while (n > 1) {
+        n >>= 1;
+        bits++;
+    }
+    return bits;
+} 
+
+uint32_t get_index(uint32_t address, uint32_t num_sets, uint32_t block_size) {
+    uint32_t offset_bits = log2_exact(block_size);
+    uint32_t index_bits  = log2_exact(num_sets);
+    uint32_t index  = (address >> offset_bits) & ((1 << index_bits) - 1);
+    return index;
 }
 
-uint32_t get_tag(uint32_t address) {
-    return address >> 12; // shift 12 bits 
+uint32_t get_tag(uint32_t address, uint32_t num_sets, uint32_t block_size) {
+    uint32_t offset_bits = log2_exact(block_size);
+    uint32_t index_bits  = log2_exact(num_sets);
+    uint32_t tag = address >> (offset_bits + index_bits);
+    return tag;
+}
+
+uint32_t get_offset(uint32_t address, uint32_t block_size){
+    uint32_t offset_bits=log2_exact(block_size);
+    uint32_t offset = address & ((1 << offset_bits) - 1);
+    return offset;
 }
