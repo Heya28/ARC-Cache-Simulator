bdce2e710c98616525cd3ceea5943de73a59e5c1
hiyat <hiyatrehan@gmail.com>
2026-08-14 23:54:34 +0000

    refactor: changed struct cache to class

--- include/Cache.hpp
+++ include/Cache.hpp
@@ -2,15 +2,19 @@
 #include "CacheLine.hpp"
 #include<vector>
 
-struct Cache{
-    std::vector<std::vector<CacheLine>> sets; // all sets in a cache 
+class Cache {
+private:
+    std::vector<CacheLine> cache_lines; // 1D vector 
     uint32_t num_sets;
+    uint32_t num_ways;
+    uint32_t block_size; 
     uint64_t hits; 
     uint64_t misses;
-    uint32_t num_ways;
-    uint32_t block_size;
-    Cache(uint32_t num_sets, uint32_t num_ways, uint32_t block_size):num_sets(num_sets), num_ways(num_ways), block_size(block_size),
-    sets(num_sets, std::vector<CacheLine>(num_ways)), hits(0), misses(0){}
-};
 
-void access(Cache& cache, uint64_t address);
+public:
+    Cache(uint32_t num_sets, uint32_t num_ways, uint32_t block_size);
+    void access(uint64_t address);
+    
+    uint64_t get_hits() const { return hits; }
+    uint64_t get_misses() const { return misses; }
+};
